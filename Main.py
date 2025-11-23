792c984c5d99ae6c32e2e73189bc4b64de0bac04
Kuntinath Noraje <58073841+kuntianth14082000@users.noreply.github.com>
2025-11-24 00:24:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,6 @@
 #All function will be called from here
 
-<<<<<<< HEAD
 #new feature
 print("Hello world")
-=======
 #new feature
->>>>>>> eb2ea3803ff22cbe55ecc6b38830cd531300ae00
-
 print("hey tendy")
